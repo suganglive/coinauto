f9b880a8feecda067bffe129b5de6915d209a035
suganglive <88217758+suganglive@users.noreply.github.com>
2022-02-02 05:59:38 +0900

    asd

--- qt.py
+++ qt.py
@@ -1,7 +1,21 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from pykorbit
+
+form_class = uic.loadUiType("mywindow.ui")[0]
+
+class MyWindow(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.inquiry)
+
+    def inquiry(self):
+        print("조회 버튼 클릭")
+
 
 app = QApplication(sys.argv)
-label = QLabel("Hello")
-label.show()
+window = MyWindow()
+window.show()
 app.exec_()
