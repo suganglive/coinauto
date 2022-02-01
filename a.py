0c616c5efd067b90e778775a03961507db3bc3a4
suganglive <88217758+suganglive@users.noreply.github.com>
2022-02-01 22:39:41 +0900

    ㄴㅁㅇ

---
+++ a.py
@@ -0,0 +1,44 @@
+# xrp_close = [800, 900, 950, 970, 980]
+# xrp_dict = {'2/21': xrp_close[0], '2/22': xrp_close[1], '2/23': xrp_close[2], '2/24': xrp_close[3], '2/25': xrp_close[4]}
+# a = list(xrp_dict.values())
+# b = sum(a) / len(a)
+# print(b)
+# print((800+900+950+970+980)/5)
+
+# porfolio = ["BTC_KRW", "BCH_KRW", "XRP_KRW", "DASH_KRW", "LTC_KRW"]
+# for i in porfolio:
+#     print(i[:3])
+
+# def get_multiply(a, b):
+#     a = a * b
+#     return a
+
+# b = get_multiply(3, 2)
+# print(b)
+
+# def cap(ticker):
+#     a = ticker.upper()
+#     return a
+
+# print(cap('hey'))
+
+# def pickup_even(numbers):
+#     a = []
+#     for i in numbers:
+#         if i%2 ==0:
+#             a.append(i)
+#     return a
+
+# b = [1, 2, 3, 4, 5, 6]
+# print(pickup_even(b))
+
+# import coin
+
+# a = coin.get_open_price("BTC")
+# print(a)
+
+# 1)  import 모듈
+# 2)  import 모듈 as 새이름
+# 3)  from 모듈 import 함수
+# 4)  from 모듈 import *
+
---
+++ b.py
@@ -0,0 +1,17 @@
+# import datetime
+
+# now = datetime.datetime.now()
+# print(now)
+
+# print(now + datetime.timedelta(hours=10))
+
+# class Book:
+#     def __init__(self, name, author, publisher):
+#         self.name = name
+#         self.author = author
+#         self.publisher = publisher
+
+# book1 = Book('Quant', 'Kang', 'media')
+# print(type(book1))
+# print(book1)
+
