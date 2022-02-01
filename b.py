ef1fad75abd4904589d52f364f8aa6f66ad470d0
suganglive <88217758+suganglive@users.noreply.github.com>
2022-02-02 05:01:27 +0900

    ads

--- b.py
+++ b.py
@@ -15,3 +15,11 @@
 # print(type(book1))
 # print(book1)
 
+# class Car:
+#     def __init__(self, model, year):
+#         self.model = model
+#         self.year = year
+
+# model3 = Car('model3', 2021)
+# print(model3.model, model3.year)
+        
