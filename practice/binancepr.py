500d24b268607e225735e9c0e19545b4d91d4c4d
suganglive <88217758+suganglive@users.noreply.github.com>
2022-02-18 12:13:02 +0900

    asd

--- practice/binancepr.py
+++ practice/binancepr.py
@@ -1,6 +1,6 @@
 import ccxt
 import datetime
-from 
+from binance.client import Client
 
 with open("/Users/sugang/Desktop/school/" + "bibi.txt")as f:
     lines = f.readlines()
@@ -8,10 +8,18 @@ with open("/Users/sugang/Desktop/school/" + "bibi.txt")as f:
     secret_key = lines[3].strip()
     binance = ccxt.binance({'apiKey': access_key, 'secret': secret_key})
 
-btc_unit = binance.fetch_balance()["BTC"]['free']
-if btc_unit > 0:
-    print("hi")
+with open("/Users/sugang/Desktop/school/" + "bibi.txt")as f:
+    lines = f.readlines()
+    access_key = lines[2].strip()
+    secret_key = lines[3].strip()
+    client = Client(access_key, secret_key)
 
-print(binance.fetch_balance()["BTC"]["total"])
-a = binance.
+# btc_unit = binance.fetch_balance()["BTC"]['free']
+# if btc_unit > 0:
+#     print("hi")
+
+# print(binance.fetch_balance()["BTC"]["total"])
+
+binance.create_limit_buy_order("ETH/USDT", 0.005136124238423859237589247298579284579284123123, 2901)
+
 
