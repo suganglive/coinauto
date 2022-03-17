b93b5d4e540b1d2444ece1e6e35ed89093e35339
suganglive <88217758+suganglive@users.noreply.github.com>
2022-03-17 09:13:38 +0900

    ,.

--- volume1.py
+++ volume1.py
@@ -8,15 +8,19 @@ def get_tickers():
     dct = {}
 
     for tick in tickers:
-        data = pyupbit.get_ohlcv(tick, count=365)
+        data = pyupbit.get_ohlcv(tick, count=6)
         try:
             vol1 = data['volume'].rolling(window=5).mean()
             close = data['close'].rolling(window=5).mean()
             vol2 = vol1 * close
+            print(vol2)
             dct[tick] = vol2[-2]
+            print(dct[tick])
         except:
             print(f"{tick}_error")
 
     sorted_d = dict(sorted(dct.items(), key=operator.itemgetter(1), reverse=True))
     a = list(sorted_d.keys())[:5]
     return a
+
+print(get_tickers())
