7234889eb8722da945c9a8d8b9747a6a7186464c
suganglive <88217758+suganglive@users.noreply.github.com>
2022-03-28 14:32:06 +0900

    asd

--- upbit15.py
+++ upbit15.py
@@ -164,7 +164,58 @@ def sell_price(ticker):
     return price
 
 def sellable(ticker):
-    price = pyupbit.get_tick_size(sell_price(ticker))
+    price = sell_price(ticker)
+    if price - 1 < 0:
+        if price * 10 - 1 < 0:
+            price = price * 10000
+            price = math.floor(price)
+            price = price - 1
+            price = price / 10000
+        else:
+            price = price * 10000
+            price = math.floor(price)
+            price = price - 1
+            price = price / 10000
+    elif len(str(math.floor(price))) == 1:
+        price = price * 100
+        price = math.floor(price)
+        price = price - 1
+        price = price / 100
+    elif len(str(math.floor(price))) == 2:
+        price = price * 10
+        price = math.floor(price)
+        price = price / 10
+    elif len(str(math.floor(price))) == 3:
+        price = math.floor(price)
+        price = price - 1
+    elif len(str(math.floor(price))) == 4:
+        if price % 10 <= 5: 
+            price = price / 10
+            price = math.floor(price)
+            price = price * 10
+        else:
+            price = price / 10
+            price = math.floor(price)
+            price = price * 10
+            price = price + 5
+    elif len(str(math.floor(price))) == 5:
+        price = price / 10
+        price = math.floor(price)
+        price = price * 10
+    elif len(str(math.floor(price))) == 6:
+        if (price/10) % 10 <= 5: 
+            price = price / 100
+            price = math.floor(price)
+            price = price * 100
+        else:
+            price = price / 100
+            price = math.floor(price)
+            price = price * 100
+            price = price + 50
+    elif len(str(math.floor(price))) >=7:
+        price = price / 1000
+        price = math.floor(price)
+        price = price * 1000
     return price
 
 def sell_limit(ticker):
