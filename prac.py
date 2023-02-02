bb59ca4b71c59457390120f1ba069018096f510d
suganglive <88217758+suganglive@users.noreply.github.com>
2023-02-03 03:32:25 +0900

    asd

--- prac.py
+++ prac.py
@@ -1,10 +1,15 @@
-import requests
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
-url = 'https://upbit.com/exchange?code=CRIX.UPBIT.KRW-NU'
-
-res = requests.get(url)
-print(res.text)
-# soup = BeautifulSoup(res.text, 'html.parser')
-# print(soup)
-
+driver = webdriver.Chrome('/Users/sugang/Documents/GitHub/coinauto/chromedriver')
+# driver.get("https://upbit.com/exchange?code=CRIX.UPBIT.KRW-WAVES")
+driver.get("https://upbit.com/exchange?code=CRIX.UPBIT.KRW-BTC")
+html = driver.page_source
+soup = BeautifulSoup(html, 'lxml')
+ans = soup.find("a", {"class": "tag tooltipDown"})
+if ans:
+    print('hi')
+    print(ans)
+else:
+    print('no')
+    print(ans)
